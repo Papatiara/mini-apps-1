708822ee073d334376063fd58a7c3ede5d08f274
Papatiara <shirazipatricia@gmail.com>
2018-05-23 09:01:25 -0700

    printing the data on the page

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,46 @@
 
+var app = {
+  server: 'http://localhost:3000/?',
 
+
+send: function(message) {
+  console.log(message)
+    $.ajax({
+      url: app.server,
+      type: 'POST',
+      data: message,
+      contentType: 'application/json',
+      success: function (data) {
+        console.log(data)
+        var result = [];
+        var splitData = data.split(',')
+        // console.log(splitData);
+        for (var i = 1; i <= splitData.length; i++) {
+          if (i % 5 === 0) {
+            result.push(splitData[i] + '\n')
+          } else {
+            result.push(splitData[i])
+          }
+        }
+      
+        console.log(result.join())
+        $("#past").append(result);
+        console.log('success!!')
+      },
+      error: function (error) {
+        console.error('error', error);
+      }
+    });
+  }
+}
+  $( document ).ready(function() {
+    console.log( "ready!" );
+  $(function(){
+    $('#target').on('click', function(e){;
+      e.preventDefault();
+         var n_input = $('#form').val();
+         console.log(n_input)
+         app.send(n_input)
+    });
+  });
+  });
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,47 @@
+const express = require('express');
+const app = express()
+app.use(express.static(__dirname + '/client'))
 
+app.listen(3000, () => console.log('Listening on port 3000!'))
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/client/index.html")
+    console.log('Hello!')
+});
+
+app.post('/', (req, res) => {
+    var arr = []
+    console.log('recebi um post')
+   var data = '';
+    req.on('data', function(chunk) {
+      data += chunk;
+      var obj = JSON.parse(data)
+      
+   var arr = ['firstName','lastName','county','city','role','sales'];
+   var parsing = function(obj) {
+       for (key in obj) {
+        if (key !== 'children') {
+          arr.push(obj[key])
+           } else if (key === 'children') {
+              if (obj.children.length > 0) {
+                 for (var i =  0; i < obj.children.length; i ++) {
+                parsing(obj.children[i]);  
+           }
+          }
+        }
+       }
+       return arr;
+      }
+parsing(obj)
+
+var str = arr.join()
+      res.send(str)
+    });
+});
+// "firstName": "Joshie",
+// "lastName": "Wyattson",
+// "county": "San Mateo",
+// "city": "San Mateo",
+// "role": "Broker",
+// "sales": 1000000,
+// "children
