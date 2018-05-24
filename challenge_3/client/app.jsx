2bef6b5a07af78a89d81b93facea590e6e73228b
Papatiara <shirazipatricia@gmail.com>
2018-05-24 09:59:26 -0700

    Saving data on database from the first form

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,115 @@
+class FormTwo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+     
+    }
+  }
+
+  render() {
+    
+  }
+}
 
 
+
+
+class FormOne extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+          name: '',
+          email: '',
+          password: '',
+          click: false
+        }
+    }
+    componentDidMount() {
+      console.log('to pronto')
+    }
+sendData(message) {
+  console.log(message)
+      fetch('http://localhost:3000/?', {
+        body: JSON.stringify(message),
+        method: 'POST',
+         headers: {
+           'Accept': 'application/json',
+           'Content-Type': 'application/json',
+         },
+          }).then(res => {
+          }).catch(err => err);
+  } 
+
+
+  getName(e) {
+   this.setState({name: e.target.value});
+  }
+
+  getEmail(e) {
+    this.setState({email: e.target.value});
+  }
+
+  getPass(e) {
+    this.setState({password: e.target.value});
+   }
+
+  next(e) {
+    e.preventDefault()
+    this.setState({click: true});
+    var obj = {}
+    obj.firstName = this.state.name,
+    obj.email = this.state.email,
+    obj.password = this.state.password
+    this.sendData(obj)
+  }
+
+render() {
+  if (this.state.click === true) {
+    return <FormTwo />
+  }
+  return (
+    <div>
+      <form>
+        <label> Name </label>
+        <input value={this.state.value} onChange={this.getName.bind(this)} type="name" />
+        <label> Email </label>
+        <input value={this.state.value} onChange={this.getEmail.bind(this)}/>
+        <label> Password </label>
+        <input value={this.state.value} onChange={this.getPass.bind(this)} />
+        <button onClick={this.next.bind(this)}>next</button>
+      </form>
+    </div> 
+  )
+ }
+}
+
+class App extends React.Component {
+  constructor(props) {
+      super(props);
+
+      this.state = {
+        check: false,
+      }
+  };
+    checkout() {
+      this.setState({
+        check: true,
+      })     
+    }
+
+render() {
+  if (this.state.check === true)  {
+    return  <FormOne />
+  }
+  return (
+    <div>  
+      <button onClick={() => this.checkout()}>checkout</button>
+    </div>
+    )
+  }
+}
+
+
+ReactDOM.render(<App />, document.getElementById('here'));
+
+window.App = App
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,42 @@
+var express = require('express')
+var bodyParser = require('body-parser');
+var mongodb = require("mongodb");
+var mongoose = require("mongoose");
 
+var app = express()
 
+mongodb.connect("mongodb://localhost:27017/?");
+mongoose.connect("mongodb://localhost:27017/patricia-db");
+
+var router = express.Router()
+app.use(express.static(__dirname + '/public'));
+app.use(express.static('/index.html'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true})); 
+
+var form1 = new mongoose.Schema({
+    firstName: String,
+    email: String,
+    password: String
+  });
+
+var User = mongoose.model("User", form1);
+
+User.create({
+    firstName: 'Patricia',
+    email: 'shirazipatricia@gmail.com',
+    password: '1234'
+})
+
+app.post('/', function(req, res) {
+  var myData = new User(req.body);
+    myData.save()
+      .then(item => {
+         res.send("item saved to database");
+    })
+      .catch(err => {
+        res.status(400).send("unable to save to database");
+      });
+});
+
+app.listen(3000, () => console.log('listening to 3000'))
