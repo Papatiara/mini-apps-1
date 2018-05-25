cf267919057edda9ff16d40facd9f768b68cb884
Papatiara <shirazipatricia@gmail.com>
2018-05-24 17:15:17 -0700

    Basic Requirements

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,17 +1,205 @@
-class FormTwo extends React.Component {
+class FormThree extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-     
+      click: false,
+      Billing: '',
+      CardNum: '',
+      CVV: '',
+      Date: '',
     }
   }
 
+getBilling(e) {
+  this.setState({ 
+    Billing: e.target.value
+  })
+}
+
+getCardNum(e) {
+  this.setState({
+    CardNum: e.target.value
+  })
+}
+
+getCVV(e) {
+  this.setState({
+    CVV: e.target.value
+  })
+}
+
+getDate(e) {
+  this.setState({
+    Date: e.target.value
+  })
+}
+
+sendData(message) {
+      fetch('http://localhost:3000', {
+        body: JSON.stringify(message),
+        method: 'POST',
+         headers: {
+           'Accept': 'application/json',
+           'Content-Type': 'application/json',
+         },
+          }).then(res => {
+          }).catch(err => err);
+  } 
+
+get() {
+    fetch('http://localhost:3000/', {
+      method: 'GET',
+    }).then(res => {
+      console.log('foi')
+    }).catch(err => err);
+}
+
+  next3(e) {
+   e.preventDefault();
+   this.setState({
+     click: true
+   })
+   var obj = {
+     FirstName: this.props.data1.name,
+     Email: this.props.data1.email,
+     Password: this.props.data1.password,
+     Address: this.props.address,
+     State: this.props.state,
+     City: this.props.city,
+     ZipCode: this.props.ZipCode,
+     CreditCard: this.state.CardNum,
+     ExpiryDate: this.state.Date,
+     CVV: this.state.CVV,
+     BillingZipcode: this.state.Billing,
+   }
+   this.sendData(obj);
+  }
+
+  purchase(e) {
+    e.preventDefault();
+    console.log('hi')
+   this.setState({
+     click:true
+   })
+   this.get()
+  }
   render() {
-    
+    if (this.state.click === true) {
+      return (
+        <div>
+          <h1> Confirm your informatiom </h1>
+          <h5> Name </h5>
+          <h4>{this.props.data1.name} </h4>
+          <h5> Email </h5>
+          <h4>{this.props.data1.email} </h4>
+          <h5> Address </h5>
+          <h4>{this.props.address} </h4>
+          <h5> State </h5>
+          <h4>{this.props.state} </h4>
+          <h5> City </h5>
+          <h4>{this.props.city} </h4>
+          <h5> Zip Code </h5>
+          <h4>{this.props.ZipCode} </h4>
+          <h5> Credit Card </h5>
+          <h4>{this.state.CardNum} </h4>
+          <h5> Expiry Date </h5>
+          <h4>{this.state.Date} </h4>
+          <h5> CVV </h5>
+          <h4>{this.state.CVV} </h4>
+          <h5> Billing Zip Code </h5>
+          <h4>{this.state.Billing} </h4>
+          <button onClick={this.purchase.bind(this)}> Purchase </button>
+        </div>
+      )
+    }
+    return (
+      <div>
+        <form>
+        <label> Credit Card </label>
+        <input value={this.state.value} onChange={this.getCardNum.bind(this)} type="name" />
+        <label> Expiry Date </label>
+        <input value={this.state.value} onChange={this.getDate.bind(this)}/>
+        <label> Billing Zip Code </label>
+        <input value={this.state.value} onChange={this.getBilling.bind(this)}/>
+        <label> CVV </label>
+        <input value={this.state.value} onChange={this.getCVV.bind(this)} />
+        <button onClick={this.next3.bind(this)}>next</button>
+      </form>
+    </div>
+    )
   }
 }
 
+class FormTwo extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(props)
+    this.state = {
+        click: false,
+        address: '',
+        city: '',
+        state: '',
+        ZipCode: ''
 
+    }
+  }
+    getAddress(e) {
+      this.setState({
+        address: e.target.value})
+    }
+    
+    getCity(e) {
+      this.setState({
+        city: e.target.value})
+    }
+
+    getState(e) {
+      this.setState({
+        state: e.target.value})
+    }
+
+    getZipCode(e) {
+      this.setState({
+        ZipCode: e.target.value
+      })
+    }
+
+    next2(e) {
+      e.preventDefault()
+      this.setState({
+        click: true
+      })
+    }
+
+  render() {
+    if (this.state.click === true) {
+      return (
+      < FormThree
+      address={this.state.address}
+      state={this.state.state}
+      city={this.state.city}
+      zipcode={this.state.ZipCode}
+      data1={this.props}
+      />
+      )
+    }
+    return (
+    <div>
+    <form>
+      <label> Address </label>
+      <input value={this.state.value} onChange={this.getAddress.bind(this)} type="name" />
+      <label> City </label>
+      <input value={this.state.value} onChange={this.getState.bind(this)}/>
+      <label> State </label>
+      <input value={this.state.value} onChange={this.getCity.bind(this)}/>
+      <label> Zip Code </label>
+      <input value={this.state.value} onChange={this.getZipCode.bind(this)} />
+      <button onClick={this.next2.bind(this)}>next</button>
+    </form>
+  </div> 
+    )
+  }
+}
 
 
 class FormOne extends React.Component {
@@ -24,22 +212,6 @@ class FormOne extends React.Component {
           click: false
         }
     }
-    componentDidMount() {
-      console.log('to pronto')
-    }
-sendData(message) {
-  console.log(message)
-      fetch('http://localhost:3000/?', {
-        body: JSON.stringify(message),
-        method: 'POST',
-         headers: {
-           'Accept': 'application/json',
-           'Content-Type': 'application/json',
-         },
-          }).then(res => {
-          }).catch(err => err);
-  } 
-
 
   getName(e) {
    this.setState({name: e.target.value});
@@ -56,16 +228,15 @@ sendData(message) {
   next(e) {
     e.preventDefault()
     this.setState({click: true});
-    var obj = {}
-    obj.firstName = this.state.name,
-    obj.email = this.state.email,
-    obj.password = this.state.password
-    this.sendData(obj)
   }
 
 render() {
   if (this.state.click === true) {
-    return <FormTwo />
+    return ( <FormTwo 
+    name={this.state.name} 
+    email={this.state.email}
+    pass={this.state.password}/>
+    )
   }
   return (
     <div>
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -10,25 +10,42 @@ mongoose.connect("mongodb://localhost:27017/patricia-db");
 
 var router = express.Router()
 app.use(express.static(__dirname + '/public'));
-app.use(express.static('/index.html'))
+// app.use(express.static('/index.html'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true})); 
 
 var form1 = new mongoose.Schema({
-    firstName: String,
-    email: String,
-    password: String
-  });
+    FirstName: String,
+    Email: String,
+    Password: String,
+    Address: String,
+    State: String,
+    City: String,
+    ZipCode: String,
+    CreditCard: String,
+    ExpiryDate: String,
+    CVV:String,
+    BillingZipcode: String
+})
 
 var User = mongoose.model("User", form1);
 
 User.create({
-    firstName: 'Patricia',
-    email: 'shirazipatricia@gmail.com',
-    password: '1234'
+    FirstName: 'Patricia',
+    Email: 'shirazipatricia@gmail.com',
+    Password: '1234',
+    Address: '1371 Minna St',
+    State: 'California',
+    City: 'San Francisco',
+    ZipCode: '93121',
+    CreditCard: '182121371318',
+    ExpiryDate: '92/09/00',
+    CVV: '988',
+    BillingZipcode: '92832'
+
 })
 
-app.post('/', function(req, res) {
+app.post('/', function(req, res, next) {
   var myData = new User(req.body);
     myData.save()
       .then(item => {
@@ -38,5 +55,9 @@ app.post('/', function(req, res) {
         res.status(400).send("unable to save to database");
       });
 });
+
+app.get('/', function(req, res) {
+  res.redirect(__dirname + '/index.html')
+})
 
 app.listen(3000, () => console.log('listening to 3000'))
